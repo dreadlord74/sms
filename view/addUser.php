6ea1033247b1149a7c754f37f8380135e892854c
dreadlord <dreadlord74@yandex.ru>
2016-01-26 16:39:56 +0500

    Сделал форму для добавления новых пользователей в систему

---
+++ view/addUser.php
@@ -0,0 +1,11 @@
+<div class="center">
+    <form action="./&add=1" method="post">
+        <label>Логин пользователя(Только латинские буквы):<br /><input type="text" name="login"/></label>
+        <label>Фамилия:<input type="text" id="fam" name="fam"/></label>
+        <label>Отчество:<input type="text" id="otch" name="otch"/></label>
+        <label>Телефон(мобильный):<input type="text" name="phone"/></label>
+        <label>E-mail:<input type="text" name="mail"/></label>
+        <label>Пароль:<input type="password" name="password"></label>
+        <label>Повторите пароль:<input type="password" name="password2"></label>
+    </form>
+</div>
