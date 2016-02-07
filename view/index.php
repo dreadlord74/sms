060072dc9a0746a4ace3fe38142621d5eef3b4b7
Dreadlord <dreadlord74@yandex.ru>
2016-02-07 19:25:03 +0500

    Подключил css и js для пагинатора

--- view/index.php
+++ view/index.php
@@ -3,11 +3,13 @@
 <head>
 <meta charset="utf-8" />
 <link rel="stylesheet" type="text/css" href="<?=VIEW?>style.css"/>
+    <link rel="stylesheet" type="text/css" href="<?=VIEW?>main.css" />
 <script type="text/javascript" src="<?=VIEW?>js/jquery-1.7.2.min.js"></script>
 <script type="text/javascript" src="<?=VIEW?>js/jquery-ui-1.8.22.custom.min.js"></script>
 <script src="<?=VIEW?>js/Chart.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="<?=VIEW?>js/cancel.js"></script>
     <script type="text/javascript" src="<?=VIEW?>js/mask.js"></script>
+    <script type="text/javascript" src="<?=VIEW?>js/pagination.js"></script>
 <title><?=$title?></title>
 </head>
 
