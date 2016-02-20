960c700cdb687243ea834ce9f3cded5df41046e2
Dreadlord <dreadlord74@yandex.ru>
2016-02-20 22:56:30 +0500

    небольшие правка дизайна

--- view/send_mass.php
+++ view/send_mass.php
@@ -5,8 +5,8 @@
 </script>
 <script src="<?=VIEW?>js/sendmass.js"></script>
 <script src="<?=VIEW?>js/device_status.js"></script>
-<div class="wrapper">
-    <fieldset style="    padding: 5px 0;
+<div class="wrapper" style="width: 600px;">
+   <!-- <fieldset style="    padding: 5px 0;
     display: table;
     padding-left: 10px;">
         <legend>Устройства</legend>
@@ -17,10 +17,9 @@
                 </li>
             <?php endforeach?>
         </ul>
-    </fieldset>
-
-    <p>Тема рассылки: <input type="text" id="tema"/></p>
+    </fieldset>-->
+    <p style="margin-top: 20px;">Тема рассылки: <input type="text" id="tema"/></p>
     <p>Текст сообщения:</p>
-    <textarea id="text" rows="10" cols="100"></textarea><br />
+    <textarea id="text" rows="10" cols="80"></textarea><br />
     <input type="button" id="btn" value="Отправить"/>
 </div>
