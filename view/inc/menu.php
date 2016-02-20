7f19eedfc02ee07e1786a84589a1adbc0ff1cd5c
Dreadlord <dreadlord74@yandex.ru>
2016-02-20 23:11:05 +0500

    Пока, массовому пользователю не будет доступны функции: - Отмена отправки - Массовая загрузка - Показ добавленных

--- view/inc/menu.php
+++ view/inc/menu.php
@@ -2,13 +2,14 @@
     <ul class="top-menu">
         <li><a href="?view=add">Добавить нового</a></li>
         <li><a href="?view=send_mass">Массовая рассылка</a></li>
-        <li><a href="/cancel">Отменить отправку всех</a></li>
-        <li><a href="?view=view_ver">Показать верифицированых</a></li>
-        <li><a href="?view=add_mass">Массовая загрузка</a></li>
+
         <li><a href="?view=sended">Просмотр статусов рассылок</a></li>
         <?php
         if (($us->get_prava() == 1) or ($us->get_prava() == 3))
             echo<<<HTML
+    <li><a href="/cancel">Отменить отправку всех</a></li>
+        <li><a href="?view=view_ver">Показать верифицированых</a></li>
+        <li><a href="?view=add_mass">Массовая загрузка</a></li>
     <li><a href="?view=settings">Изменение данных</a></li>
 HTML;
         ?>
